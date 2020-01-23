640ca781154b34739bb0d3d8ed8b3fb22929b862
mohamed82008 <mohamed82008@gmail.com>
2020-01-23 16:40:39 +1100

    multi and arraydist support

--- src/compat/distributionsad.jl
+++ src/compat/distributionsad.jl
@@ -1,4 +1,10 @@
-using .DistributionsAD: TuringWishart, TuringInverseWishart
+using .DistributionsAD: TuringWishart, TuringInverseWishart, MultipleContinuousUnivariate,
+                        MatrixContinuousUnivariate, MultipleContinuousMultivariate,
+                        ProductVectorContinuousUnivariate,
+                        ProductMatrixContinuousUnivariate,
+                        ProductVectorContinuousMultivariate
+
+# TuringWishart
 
 function logpdf_with_trans(
     d::TuringWishart,
@@ -17,6 +23,8 @@ function getlogp(d::TuringWishart, Xcf, X)
     return 0.5 * ((d.df - (dim(d) + 1)) * logdet(Xcf) - tr(d.chol \ X)) - d.c0
 end
 
+# TuringInverseWishart
+
 function logpdf_with_trans(
     d::TuringInverseWishart,
     X::AbstractMatrix{<:Real},
@@ -33,4 +41,126 @@ end
 function getlogp(d::TuringInverseWishart, Xcf, X)
     Ψ = d.S
     return -0.5 * ((d.df + dim(d) + 1) * logdet(Xcf) + tr(Xcf \ Ψ)) - d.c0
+end
+
+# Multi and ArrayDist
+
+function logpdf_with_trans(
+    dist::MultipleContinuousUnivariate,
+    x::AbstractVector{<:Real},
+    istrans::Bool,
+)
+    return sum(logpdf_with_trans(dist.dist, x, istrans))
+end
+function link(
+    dist::MultipleContinuousUnivariate,
+    x::AbstractVector{<:Real},
+)
+    return link(dist.dist, x)
+end
+function invlink(
+    dist::MultipleContinuousUnivariate,
+    x::AbstractVector{<:Real},
+)
+    return invlink(dist.dist, x)
+end
+
+function logpdf_with_trans(
+    dist::MatrixContinuousUnivariate,
+    x::AbstractMatrix{<:Real},
+    istrans::Bool,
+)
+    return sum(logpdf_with_trans.(dist.dist, x, istrans))
+end
+function link(
+    dist::MatrixContinuousUnivariate,
+    x::AbstractMatrix{<:Real},
+)
+    return link(dist.dist, x)
+end
+function invlink(
+    dist::MatrixContinuousUnivariate,
+    x::AbstractMatrix{<:Real},
+)
+    return invlink(dist.dist, x)
+end
+
+function logpdf_with_trans(
+    dist::MultipleContinuousMultivariate,
+    x::AbstractMatrix{<:Real},
+    istrans::Bool,
+)
+    return sum(logpdf_with_trans(dist.dist, x, istrans))
+end
+function link(
+    dist::MultipleContinuousMultivariate,
+    x::AbstractMatrix{<:Real},
+)
+    return link(dist.dist, x)
+end
+function invlink(
+    dist::MultipleContinuousMultivariate,
+    x::AbstractMatrix{<:Real},
+)
+    return invlink(dist.dist, x)
+end
+
+function logpdf_with_trans(
+    dist::ProductVectorContinuousMultivariate,
+    x::AbstractMatrix{<:Real},
+    istrans::Bool,
+)
+    return sum(logpdf_with_trans.(dist.dists, [x[:,i] for i in 1:size(x, 2)], istrans))
+end
+function link(
+    dist::ProductVectorContinuousMultivariate,
+    x::AbstractMatrix{<:Real},
+)
+    return reduce(hcat, link.(dist.dists, [x[:,i] for i in 1:size(x, 2)]))
+end
+function invlink(
+    dist::ProductVectorContinuousMultivariate,
+    x::AbstractMatrix{<:Real},
+)
+    return reduce(hcat, invlink.(dist.dists, [x[:,i] for i in 1:size(x, 2)]))
+end
+
+function logpdf_with_trans(
+    dist::ProductVectorContinuousUnivariate,
+    x::AbstractVector{<:Real},
+    istrans::Bool,
+)
+    return sum(logpdf_with_trans.(dist.dists, x, istrans))
+end
+function link(
+    dist::ProductVectorContinuousUnivariate,
+    x::AbstractVector{<:Real},
+)
+    return link.(dist.dists, x)
+end
+function invlink(
+    dist::ProductVectorContinuousUnivariate,
+    x::AbstractVector{<:Real},
+)
+    return invlink.(dist.dists, x)
+end
+
+function logpdf_with_trans(
+    dist::ProductMatrixContinuousUnivariate,
+    x::AbstractMatrix{<:Real},
+    istrans::Bool,
+)
+    return sum(logpdf_with_trans.(dist.dists, x, istrans))
+end
+function link(
+    dist::ProductMatrixContinuousUnivariate,
+    x::AbstractMatrix{<:Real},
+)
+    return link.(dist.dists, x)
+end
+function invlink(
+    dist::ProductMatrixContinuousUnivariate,
+    x::AbstractMatrix{<:Real},
+)
+    return invlink.(dist.dists, x)
 end
